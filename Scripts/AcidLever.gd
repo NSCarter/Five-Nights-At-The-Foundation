9da1e73cf6ddc0e6855d9db0434db81141c57b3a
Niamh Carter <niamh.carter13@gmail.com>
2022-08-06 17:57:43 +0100

    Acid raises on button press

---
+++ Scripts/AcidLever.gd
@@ -0,0 +1,15 @@
+extends TextureButton
+
+var button_down = false
+
+func _process(delta):
+	if button_down:
+		Stats.acid_pos -= 0.75
+
+
+func _on_AcidLever_button_down():
+	button_down = true
+
+
+func _on_AcidLever_button_up():
+	button_down = false
