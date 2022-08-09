ac210871cd815b829d763a008435e34e7e39f2db
Niamh Carter <niamh.carter13@gmail.com>
2022-08-09 20:51:42 +0100

    Tidy code layout

--- Scripts/AcidLever.gd
+++ Scripts/AcidLever.gd
@@ -6,10 +6,8 @@ func _process(delta):
 	if button_down:
 		Stats.acid_pos -= 0.75
 
-
 func _on_AcidLever_button_down():
 	button_down = true
-
 
 func _on_AcidLever_button_up():
 	button_down = false
