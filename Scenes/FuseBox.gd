84bae06b427d8f1d5b92991735dd9ac8ca8cdfec
Niamh Carter <niamh.carter13@gmail.com>
2022-10-09 21:58:30 +0100

    Fuses can be removed

--- Scenes/FuseBox.gd
+++ Scenes/FuseBox.gd
@@ -17,3 +17,23 @@ func _on_FuseBox_pressed():
 		$Fuse3.visible = true
 		$Fuse4.visible = true
 		open = true
+
+
+func _on_Fuse1_pressed():
+	fuse_pressed($Fuse1)
+
+
+func _on_Fuse2_pressed():
+	fuse_pressed($Fuse2)
+
+
+func _on_Fuse3_pressed():
+	fuse_pressed($Fuse3)
+
+
+func _on_Fuse4_pressed():
+	fuse_pressed($Fuse4)
+
+
+func fuse_pressed(fuse):
+	fuse.texture_normal = load("res://Assests/Images/SCP 017/FuseEmpty.png")
